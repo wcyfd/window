3b96e535a846069ccef92df9d56c0d632171018d
wcy <1101697691@qq.com>
2019-12-27 10:17:07 +0800

    去掉额外的引用

--- src/main/java/game/quick/window/GameWindows.java
+++ src/main/java/game/quick/window/GameWindows.java
@@ -5,7 +5,6 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PipedInputStream;
 import java.io.PipedOutputStream;
